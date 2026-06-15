1d7c6a25ade24aad417f67c5371087d8747ee58b
shubham-codeking <shubhampal2008@gmail.com>
2026-06-15 21:14:57 +0530

    Added power calculation module

---
+++ src/power.cpp
@@ -0,0 +1,11 @@
+#include "power.h"
+
+double calcPowerVR(const double &voltage, const double &resistance){
+    return (voltage*voltage/resistance);
+}
+double calcPowerVI(const double &voltage, const double &current){
+    return (voltage*current);
+}
+double calcPowerIR(const double &current, const double &resistance){
+    return (current*resistance*resistance);
+}
---
+++ src/power.h
@@ -0,0 +1,3 @@
+double calcPowerVR(const double &voltage, const double &resistance);
+double calcPowerIR(const double &current, const double &resistance);
+double calcPowerVI(const double &voltage, const double &current);
