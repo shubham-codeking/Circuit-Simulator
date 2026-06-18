9b593e1ec3d360af1816972e10b7d50fdd1542f9
shubham-codeking <shubhampal2008@gmail.com>
2026-06-18 15:18:35 +0530

    Added expression tokenizer

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,5 +1,8 @@
 #include "utilities.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
 using namespace std;
 
 bool zeroDivisionCheck(const double &quantity){
@@ -10,4 +13,53 @@ bool zeroDivisionCheck(const double &quantity){
     else{
         return false;
     }
+}
+
+vector<string> tokenizer(string &expression){
+    expression = "("+expression+")";
+    string element = "";
+    bool state = true;
+    vector<string> tokenArray = {};
+    vector<char> operatorArray = {'(',')','+'};
+    for(int i=0; i<expression.length(); i++){
+        if(state){
+            element="";
+        }
+        for(char op: operatorArray){
+            if(expression[i]==op){
+                if(!state){
+                    tokenArray.push_back(element);
+                }
+                element=op;
+                tokenArray.push_back(element);
+                state = true;
+                break;
+            }
+        }
+        if(isalnum(expression[i])){
+            element+=expression[i];
+            if(state){
+                state = false;
+            }
+            continue;
+        }
+        else if(expression[i]=='|'){
+            if(state){
+                element+=expression[i];
+                state = false;
+            }
+            else{
+                if(element=="|"){
+                    tokenArray.push_back("||");
+                    state = true;
+                }
+                else{
+                    tokenArray.push_back(element);
+                    element="";
+                    element+=expression[i];
+                }
+            }
+        }
+    }
+    return tokenArray;
 }
--- src/utilities.h
+++ src/utilities.h
@@ -1,6 +1,9 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
+#include <vector>
+#include <string>
 
 bool zeroDivisionCheck(const double &quantity);
+vector<string> tokenizer(string &expression);
 
 #endif
